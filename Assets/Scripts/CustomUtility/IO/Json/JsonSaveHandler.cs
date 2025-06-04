62158b8d7602e0b8de85b2dfd0b76e97e3af081f
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-06-04 14:14:08 +0900

    [Feat] Add Save system, UI
    - json으로 세이브 파일을 읽기, 쓰기 가능하게 구현
    - UI로 세이브 파일을 선택, 취소 가능함

--- Assets/Scripts/CustomUtility/IO/Json/JsonSaveHandler.cs
+++ Assets/Scripts/CustomUtility/IO/Json/JsonSaveHandler.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class JsonSaveHandler : MonoBehaviour
+public abstract class JsonSaveHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public static string basePath
     {
-        
+        get
+        {
+            return Path.Combine(Application.dataPath, "SaveData");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Save<T>(T target) where T : SaveData
     {
+        Directory.CreateDirectory(basePath);
         
+        string saveFilePath = GetFilePath(basePath);
+        string jsonString = JsonUtility.ToJson(target);
+
+        if (IsFileEmpty(jsonString)) return;
+        
+        File.WriteAllText(saveFilePath, jsonString);
+        IsFileAccessible(saveFilePath);
+    }
+    // 지정제한자로 new()를 추가하는 이유는,
+    // JsonUtility가 매개변수가 없는 생성자를 필요로 하기 때문
+    public void Load<T>(ref T target) where T : SaveData, new()
+    {
+        string filePath = GetFilePath(target.GetType().ToString());
+        string jsonString = File.ReadAllText(filePath);
+
+        if(!IsFileAccessible(filePath)) return;
+        if (IsFileEmpty(jsonString)) return;
+
+        target = JsonUtility.FromJson<T>(jsonString);
+    }
+    private bool IsFileEmpty(string jsonString)
+    {
+        if(string.IsNullOrEmpty(jsonString))
+        {
+            Debug.LogError("Save File Empty");
+            return true;
+        }
+        return false;
+    }
+    private bool IsFileAccessible(string filePath)
+    {
+        if(File.Exists(filePath))
+        {
+            Debug.Log($"Save File Exists at path: {filePath}");
+            return true;
+        }
+        else
+        {
+            Debug.LogError($"cannot access to save file at path: {filePath}");
+            return false;
+        }
+    }
+    protected static string GetFilePath(string fileName)
+    {
+        return Path.Combine(basePath, $"{fileName}.json");
     }
 }
